a251187c9c497dbe89646bdea44b55f436b044dc
shabeeb k <shabeeb.k@ramzaula.com>
2025-05-04 18:14:07 +0300

    suppliers list started

---
+++ app/suppliers/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SuppliersPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Suppliers Page</h1>
+            <p>Welcome to the Suppliers page!</p>
+        </div>
+    );
+};
+
+export default SuppliersPage;
