40ea59154cede6789e664aed09105ba8207e3912
shabeeb k <shabeeb.k@ramzaula.com>
2025-05-04 16:34:49 +0300

    product list page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,6 @@
 import Filter from "@/components/Products/Filter";
+import ProductCard from "@/components/Products/ProductCard";
+import SearchBar from "@/components/Products/SearchBar";
 import Image from "next/image";
 import React from "react";
 
@@ -97,22 +99,11 @@ const ProductsPage: React.FC = () => {
           />
         </div>
         <div className="col-span-9">
-          <div className="relative">
-            <div className="w-10 h-10 bg-gradient-to-r from-[var(--green-gradient-from)] via-[var(--green-gradient-via)] to-[var(--green-gradient-to)] rounded-full flex justify-center items-center absolute top-2 right-2">
-              <Image
-                src="/icons/search.svg"
-                alt="Arrow Icon"
-                width={20}
-                height={20}
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                placeholder="Search Polymers"
-                className="w-full px-4 py-4 rounded-full border-1 border-[var(--green-light)]"
-              />
-            </div>
+          <SearchBar />
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+            {products.map((product, index) => (
+              <ProductCard key={index} product={product} />
+            ))}
           </div>
         </div>
       </div>
---
+++ components/Products/SearchBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+
+interface SearchBarProps {
+  onSearch?: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [query, setQuery] = useState("");
+
+  //   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //     setQuery(event.target.value);
+  //   };
+
+  //   const handleSearch = () => {
+  //     onSearch(query);
+  //   };
+
+  return (
+    <div className="relative">
+      <div className="w-10 h-10 bg-gradient-to-r from-[var(--green-gradient-from)] via-[var(--green-gradient-via)] to-[var(--green-gradient-to)] rounded-full flex justify-center items-center absolute top-2 right-2">
+        <Image
+          src="/icons/search.svg"
+          alt="Arrow Icon"
+          width={20}
+          height={20}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Search Polymers"
+          className="w-full px-4 py-4 rounded-full border-1 border-[var(--green-light)]"
+          //   value={query}
+          //   onChange={handleSearch}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
