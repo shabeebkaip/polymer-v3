63a3bb9cd161eb147e9a95978bfc99b468c04134
Shabeeb Kaip <shabeebkaip@gmail.com>
2025-07-14 11:01:31 +0300

    deleted errors

--- test-import.js
+++
@@ -1,4 +0,0 @@
-// Temporary test file to check store exports
-const { useProductRequestDetailStore } = require('./stores/user.ts');
-
-console.log('Store exported successfully:', typeof useProductRequestDetailStore);
