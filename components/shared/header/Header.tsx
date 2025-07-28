94f36ad2aaf021ec9421e7938f48b3e421ceec82
Shabeeb Kaip <shabeebkaip@gmail.com>
2025-07-28 19:16:10 +0300

    removed extra close icon

--- components/shared/header/Header.tsx
+++ components/shared/header/Header.tsx
@@ -172,24 +172,7 @@ const Header: React.FC = () => {
                                 </div>
                                 <span className="font-semibold text-gray-900">Menu</span>
                             </div>
-                            <button
-                                onClick={toggleMenu}
-                                className="p-2 text-gray-500 hover:text-[var(--green-main)] hover:bg-gray-50 rounded-lg transition-all duration-200"
-                            >
-                                <svg
-                                    className="w-5 h-5"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth={2}
-                                        d="M6 18L18 6M6 6l12 12"
-                                    />
-                                </svg>
-                            </button>
+
                         </div>
 
                         {/* Mobile Menu Content */}
