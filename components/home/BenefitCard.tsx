55d8883502ce0a7dca56a3b28005e8c0cffcbaa3
Shabeeb Kaip <shabeebkaip@gmail.com>
2025-11-13 00:19:27 +0300

    benefits update

--- components/home/BenefitCard.tsx
+++ components/home/BenefitCard.tsx
@@ -10,7 +10,7 @@ const BenefitCard: React.FC<BenefitCardProps> = ({
   return (
     <div className="flex flex-col items-start justify-between gap-6 p-8 md:p-10 rounded-2xl border border-gray-100  shadow-lg hover:shadow-xl transition-all duration-300 group min-h-[340px] w-full">
       <div className="w-full">
-        <h3 className="text-xs font-semibold text-green-700 uppercase tracking-wide mb-1">
+        <h3 className="text-xs font-semibold text-primary-600 uppercase tracking-wide mb-1">
           {subtitle}
         </h3>
         <h2 className="text-xl md:text-2xl font-bold text-gray-900 mb-4 leading-snug">
@@ -22,8 +22,8 @@ const BenefitCard: React.FC<BenefitCardProps> = ({
               key={index}
               className="flex items-center gap-3 text-gray-700 text-sm md:text-base "
             >
-              <span className="inline-flex items-center justify-center w-6 h-6 rounded-full bg-green-50">
-                <Check className="w-10 h-10 text-green-600" />
+              <span className="inline-flex items-center justify-center w-6 h-6 rounded-full bg-primary-50">
+                <Check className="w-10 h-10 text-primary-500" />
               </span>
               <span>{item}</span>
             </li>
@@ -32,7 +32,7 @@ const BenefitCard: React.FC<BenefitCardProps> = ({
       </div>
       <a
         href={registerLink}
-        className="inline-flex items-center gap-2 text-sm md:text-base px-5 py-2.5 rounded-lg bg-gradient-to-r from-green-600 to-emerald-600 text-white font-semibold shadow hover:from-green-700 hover:to-emerald-700 transition-all duration-200 focus:ring-2 focus:ring-green-300"
+        className="inline-flex items-center gap-2 text-sm md:text-base px-5 py-2.5 rounded-lg bg-primary-500 hover:bg-primary-600 text-white font-semibold shadow transition-all duration-200 focus:ring-2 focus:ring-primary-500/30"
       >
         Register Now
         <svg
